a3338500f0c0c4bc8d94af3ff51b641dcdf28176
Command <67428649+Miles-coder2000@users.noreply.github.com>
2025-06-19 12:05:39 +0800

    New Changes
    Migrate to Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        gray: {
-          900: '#121212',
-          800: '#1f1f1f',
-          700: '#2d2d2d',
-          600: '#404040',
-        }
-      },
-      aspectRatio: {
-        '16/7': '16 / 7',
-      },
-      screens: {
-        'xs': '475px',
-        mobile: {max: "767px"}
-      },
-      spacing: {
-        'safe-area-bottom': 'env(safe-area-inset-bottom, 0px)',
-      },
-    },
-  },
-  plugins: [
-   
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  theme: {
+    screens:{
+      'sm':{'max':'639px'},
+      // 'md':'420px',
+      // 'lg':'1024px',
+      // 'xl':'1280px',
+      // '2xl':'1536px',
+
+    },
+    extend: {},
+  },
+  plugins: [],
 }
+
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // vite.config.js
-  server: {
-    // Expose on all network interfaces
-    host: '0.0.0.0',
-    // History API Fallback
-    historyApiFallback: true,
-  }
 })
